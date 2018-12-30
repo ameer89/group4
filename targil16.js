ca8ff1cce7cbb1f99184d37e8d2a405a5fde877f
ameer89 <44411703+ameer89@users.noreply.github.com>
2018-12-30 20:41:57 +0200

    Delete targil16.js

--- targil16.js
+++
@@ -1,5 +0,0 @@
-
-var BooksArray = [{title : "The Hobbit", author  : "J.R.R. Tolkien",alreadyRead : 1},{title : "The Lord of the Rings", author  : "J.R.R. Tolkien.",alreadyRead : 0}];
-BooksArray.forEach(book => {
-    book.alreadyRead === 1 ? console.log("You already read " + book.title + " by " + book.author ) : console.log("You still need to read " + book.title + " by " + book.author);
-});
