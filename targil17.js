6313f07c61032000487452dfe27c1241cb2f873a
ameer89 <44411703+ameer89@users.noreply.github.com>
2019-01-06 18:56:18 +0200

    Delete targil17.js

--- targil17.js
+++
@@ -1,8 +0,0 @@
-var myMovie = {title : "Puff the Magic Dragon", duration : 30, stars : ["Puff","Jackie","Living Sneezes."]};
-
-function PrintMovie(movie) {
-    for (var i = 0, starsString = "" ; i < movie.stars.length; i++) { starsString += movie.stars[i] + " " }    
-    console.log(movie.title + " lasts for " + movie.duration + " minutes. Stars: " + starsString);
-}
-
-PrintMovie(myMovie);
